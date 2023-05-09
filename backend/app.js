abeacb3f417e2ec8bb59626326891054935b21f3
kaan.bas <baskaan8@gmail.com>
2023-05-09 16:34:01 +0300

    Mongo DB Added

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,43 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const mongoose = require("mongoose");
+
+const username ='TEST' ;
+const password = 'TEST'; 
+
+async function main() {
+  const url =
+    `mongodb+srv://${username}:${password}@cluster0.cxaro1z.mongodb.net/?retryWrites=true&w=majority`;
+  const client = new mongoose(url);
+  try {
+    await client.connect();
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+main().catch(console.error);
+
+// Add the cors middleware before your API routes
+app.use(cors());
+
+let aa = [1, 2, 3, 4, 5, 6];
+
+app.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      data: aa,
+      message: "Başarılı",
+    })
+  );
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,16 +1,29 @@
 <template>
-  <div class="home">
- Home
+  <div class="home">Home
+    <h1>{{ this.APIdata }}</h1>
   </div>
+ 
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-
+import axios from "axios";
+import { Options, Vue } from "vue-class-component";
 
 @Options({
-  components: {
-  
+  components: {},
+  data:()=>({
+    APIdata:[]
+  }),
+  methods: {
+    getData() {
+      axios.get("http://localhost:3000").then((res) => {
+        console.log(res)
+        this.APIdata = res.data 
+        });
+    },
+  },
+  mounted() {
+    this.getData();
   },
 })
 export default class HomeView extends Vue {}
